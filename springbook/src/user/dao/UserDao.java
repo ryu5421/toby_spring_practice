69cd09d2830d45193cccd23500c8ae1f59c1f310
ryu5421 <grafeisen.0@gmail.com>
2023-03-27 17:43:16 +0900

    test

--- springbook/src/user/dao/UserDao.java
+++ springbook/src/user/dao/UserDao.java
@@ -79,22 +79,22 @@ public abstract class UserDao {
 	}
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		UserDao dao = new UserDao();
+//		UserDao dao = new UserDao();
 		
-		User user = new User();
-		user.setId("whiteShip");
-		user.setName("백기선");
-		user.setPassword("married");
-		
-		dao.add(user);
-		
-		System.out.println(user.getId() + " 등록 성공");
-		
-		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		
-		System.out.println(user2.getPassword());
-		
-		System.out.println(user2.getId()+" 조회 성공");
+//		User user = new User();
+//		user.setId("whiteShip");
+//		user.setName("백기선");
+//		user.setPassword("married");
+//		
+//		dao.add(user);
+//		
+//		System.out.println(user.getId() + " 등록 성공");
+//		
+//		User user2 = dao.get(user.getId());
+//		System.out.println(user2.getName());
+//		
+//		System.out.println(user2.getPassword());
+//		
+//		System.out.println(user2.getId()+" 조회 성공");
 	}
 }
